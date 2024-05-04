3c0a192c5cbbe046fb5df5cdb33a734f76e761b1
kajal panchal <kajaljpan@gmail.com>
2024-05-04 11:00:59 -0700

    sending index and show data

---
+++ server.js
@@ -0,0 +1,29 @@
+// DEPENDENCIES
+const express = require('express')
+const breads = express.Router()
+
+//INDEX
+breads.get('/', (req, res) => {
+    res.send('This is the index at / breads')
+})
+module.exports = breads 
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+// Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
