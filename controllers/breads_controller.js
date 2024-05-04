f68cb8f103ef429a42a032fafe0982c0b7f23e12
kajal panchal <kajaljpan@gmail.com>
2024-05-04 13:11:33 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,15 +2,21 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// INDEX
-breads.get('/', (req, res) => {
-  res.send(Bread)
-})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
   })
   
+  // INDEX
+breads.get('/', (req, res) => {
+  res.render('Index',
+    {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
+})
+
 
 module.exports = breads
---
+++ views/index.jsx
@@ -0,0 +1,29 @@
+const React = require('react')
+const Default = require('./layouts/default')
+
+function Index({breads}) {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        {/*This is a JSX comment. */}
+        {/*<p>I have {breads[0] .name} bread!</p> */}
+        <ul>
+          {
+            breads.map((bread, index) => {
+              return (<li key={index}>
+              <a>
+                   {bread.name}
+              </a>
+              </li>)
+            
+            })
+          }
+        </ul>
+      </Default>
+    )
+}
+
+
+
+
+module.exports = Index
